6c32f14322022a1839a51d3c167b13d895f2e26f
SHADOW <unsterblich.wang@gmail.com>
2025-04-23 19:29:47 +0800

    提交分布式服务端初版

---
+++ GateServer/GateServer/CServer.h
@@ -0,0 +1,17 @@
+#ifndef _CSERVER_H_
+#define _CSERVER_H_
+
+#include "const.h"
+
+class CServer : public std::enable_shared_from_this<CServer> {
+public:
+    CServer(boost::asio::io_context &ioc, unsigned short &port);
+    void Start();
+
+private:
+    tcp::acceptor _acceptor;
+    net::io_context &_ioc;
+    //boost::asio::ip::tcp::socket _socket;
+};
+
+#endif
